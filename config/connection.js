eec9d46ff37403f7bd8f61070fc1cf31ac6a21c2
Peter Boss <m@Peters-MacBook-Pro.local>
2018-03-06 22:10:43 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection(process.env.JAWSDB_URL || {
-  port: 3306,
   host: 'localhost',
   user: 'root',
   password: '',
@@ -11,6 +10,11 @@ const connection = mysql.createConnection(process.env.JAWSDB_URL || {
 connection.connect(err => {
   if (err) throw err;
   console.log('connected as id ' + connection.threadId);
+  const sql = ('CREATE TABLE burgers (id int auto_increment primary key, burger_name varchar(255), devoured boolean default false');
+  connecton.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log('Table created');
+  });
 });
 
 module.exports = connection;
