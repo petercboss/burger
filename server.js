bb0820acb53299384f4a1af2e89ce8ded7e3843c
Peter Boss <m@Peters-MacBook-Pro.local>
2018-03-06 19:15:53 -0600

    changed static

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 
 const app = express();
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
