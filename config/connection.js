404f63f99e0f8ca219b774ebdfebc3907c9e5b99
Peter Boss <m@Peters-MacBook-Pro.local>
2018-03-07 11:45:58 -0600

    added create table

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,8 @@ const connection = mysql.createConnection(process.env.JAWSDB_URL || {
 connection.connect(err => {
   if (err) throw err;
   console.log('connected as id ' + connection.threadId);
-  const sql = ('CREATE TABLE burgers (id int auto_increment primary key, burger_name varchar(255), devoured boolean default false');
-  connecton.query(sql, (err, result) => {
+  const sql = ('CREATE TABLE burgers (id int auto_increment primary key, burger_name varchar(255) not null, devoured boolean default false');
+  connection.query(sql, (err, result) => {
     if (err) throw err;
     console.log('Table created');
   });
